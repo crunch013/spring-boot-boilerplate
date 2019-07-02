5ec34ffa4ce5792559be275af11f72fff741a32d
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2019-07-02 11:03:45 -0300

    Initial boilerplate

---
+++ core/src/main/java/com/github/crunch013/core/Application.java
@@ -0,0 +1,14 @@
+package com.github.crunch013.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
